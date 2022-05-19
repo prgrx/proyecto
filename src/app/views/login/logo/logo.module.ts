3241a808ea9ad9cfb14617d9b57db9ef44b9078e
Fernando <ferenandoruiz@gmail.com>
2022-05-19 23:41:06 +0200

    añado el logo

---
+++ src/app/views/login/logo/logo.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { LogoPageRoutingModule } from './logo-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    LogoPageRoutingModule
+  ],
+  declarations: []
+})
+export class LogoPageModule {}
