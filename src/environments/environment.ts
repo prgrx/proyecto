45f189e0e95b72f08c0626fa8be8976b7afcfcf9
Pedro Vargas <pvargas@idento.es>
2022-06-01 21:49:36 +0200

    Environments setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: "AIzaSyByuaUJlQAi5leuajupZDVKQELYyOOJ96c",
     authDomain: "proyecto-hlanz.firebaseapp.com",
