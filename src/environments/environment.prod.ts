0a3827b135bbfb8873cca28ea76966b6d804549b
Pedro Vargas <pvargas@idento.es>
2022-06-01 21:49:16 +0200

    Enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyByuaUJlQAi5leuajupZDVKQELYyOOJ96c",
+    authDomain: "proyecto-hlanz.firebaseapp.com",
+    projectId: "proyecto-hlanz",
+    storageBucket: "proyecto-hlanz.appspot.com",
+    messagingSenderId: "269514279164",
+    appId: "1:269514279164:web:13708ddfa1248bc9bd2f79",
+    measurementId: "G-97HBCSNC03"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyByuaUJlQAi5leuajupZDVKQELYyOOJ96c",
     authDomain: "proyecto-hlanz.firebaseapp.com",
